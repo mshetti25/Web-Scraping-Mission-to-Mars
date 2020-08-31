c582e051bfc530c5db19e630cf0f0e9df8776bc1
Manisha Shetti <manishashetti@Manishas-MacBook-Pro-2.local>
2020-08-31 10:57:01 -0500

    Final changes

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -5,24 +5,27 @@ import scrape_mars
 #import pdb;pdb.set_trace()
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
-
+app.config["MONGO_URI"]= "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
 @app.route("/")
-
 def index():
+    #import pdb; pdb.set_trace()
     mars = mongo.db.mars.find_one()
     return render_template("index.html", mars = mars)
 
-
-
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars 
-    mars_data = scrape_mars.scrape()
-    print(mars_data)
-    mars.update({}, mars_data, upsert=True)
-    return redirect("/", code=302)
+    mars = mongo.db.mars
+    data = scrape_mars.scrape()
+    mars.update(
+        { },
+        data,
+        upsert=True
+    )
+    
+    
+    return redirect('http://localhost:5000/', code=302)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -3,52 +3,62 @@ from splinter import Browser as b
 from bs4 import BeautifulSoup as bs
 import time
 
-    
+executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+browser = b("chrome", **executable_path, headless=False)
+
 
 
 def scrape():
-    executable_path = {'executable_path': 'chromedriver.exe'}
-    browser =  b('chrome', **executable_path)
+    data = {}
+    news = mars_news()
+    data["mars_news"] = news[0]
+    data["mars_paragraph"] = news[1]
+    data["mars_image"] = mars_image()
+    data["mars_facts"] = mars_facts()
+    data["mars_hemisphere"] = mars_hem_data()
+    browser.quit()
+    return data
 
-    data= {}
-
-    # NASA Mars News
-    browser.visit('https://mars.nasa.gov/news/')
+# NASA Mars News
+def mars_news():
+    url = "https://mars.nasa.gov/news/"
+    browser.visit(url)
+    time.sleep(5)
     html = browser.html
     soup = bs(html, 'html.parser')
-    title = soup.find('div', class_="bottom_gradient").text
-    para = soup.find('div', class_='article_teaser_body').text
-    data["News_title"] = title
-    data["New_para"] = para
+    title = soup.find("div",class_="content_title").text
+    para = soup.find("div", class_="article_teaser_body").text
+    news_data = [title, para]
+    return news_data
 
-    # JPL Mars Space Images - Featured Image
-    browser.visit('https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars')
-    browser.click_link_by_partial_text('FULL IMAGE')
+# JPL Mars Space Images - Featured Image
+def  mars_image():
+    space_image_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(space_image_url)
     time.sleep(5)
-    browser.click_link_by_partial_text('more info')
-    time.sleep(5)
-    html = browser.html
-    soup = bs(html,'html.parser')
-    i_path= soup.find('figure',class_='lede').a['href']
-    featured_image_url = 'https://www.jpl.nasa.gov/'+ i_path
-    data["Featured_image_url"] = featured_image_url
+    space_image_html = browser.html
+    soup = bs(space_image_html,"html.parser")
+    i_path = soup.find('img', class_ = 'thumb')['src']
+    featured_image_url = "https://www.jpl.nasa.gov" + i_path
+    return featured_image_url
 
-    # Mars Facts
+# Mars Facts
+def  mars_facts():
     facts_df = pd.read_html('https://space-facts.com/mars/')[0]
     facts_df.columns=['Labels', 'Value']
     facts_html = facts_df.to_html(header = False, index = False)
-    data["Mars_table"] = facts_html
+    return facts_html
 
     # Mars Hemispheres
+def mars_hem_data():
     browser.visit('https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars')
+    time.sleep(5)
     html = browser.html
     soup = bs(html,'html.parser')
     img = soup.find('div', class_='collapsible results')
     hem_img = []
 
-    # print(len(images.find_all("div", class_="item")))
     for x in range(len(img.find_all("div", class_="item"))):
-        # print(i)
         time.sleep(5)
         h_img = browser.find_by_tag('h3')
         h_img[x].click()
@@ -64,9 +74,8 @@ def scrape():
             }
         hem_img.append(hem_dict)
         browser.back()
-        data["hemispheres_image_urls"] = hem_img
-
-    return data
+    return hem_img  
 
 if __name__ == "__main__":
+    print('Starting the app...')
     scrape()
